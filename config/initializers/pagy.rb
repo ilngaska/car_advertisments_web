a0e8c24b7a1b7e9cf89892a689aa961f839c2ce3
Markie <markie@example.com>
2026-03-03 13:55:37 +0200

    Initial rails scaffold

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,7 +3,5 @@ require 'pagy'
 require 'pagy/extras/overflow'
 require 'pagy/extras/array' 
 
-# require 'pagy/extras/tailwindcss'
-
 Pagy::DEFAULT[:items] = 10
 Pagy::DEFAULT[:overflow] = :last_page
