eea851eea8de1de10cfd71a99c7d1a91358998a4
Markie <markie@example.com>
2026-03-06 13:24:38 +0200

    Add user registration and fix localization on home page
    Implemented UsersController, updated signup/login routes, and integrated I18n translations into the main dashboard.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -6,16 +8,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
       redirect_to root_path, notice: t('alerts.signup_success')
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_content
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.expect(user: %i[email password password_confirmation])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get    'login',  to: 'sessions#new',     as: :login
+  get    'login',  to: 'sessions#new', as: :login
   post   'login',  to: 'sessions#create'
   get 'logout', to: 'sessions#destroy', as: :logout
 
-  get    'signup', to: 'users#new',        as: :signup
+  get    'signup', to: 'users#new', as: :signup
   post   'signup', to: 'users#create'
 
   resources :cars, only: [:index] do
